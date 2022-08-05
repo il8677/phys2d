2539e4d344486ae083a78bdbf50f9afd3b964dd4
il8677 <jiajiaisaac@gmail.com>
2022-08-05 02:45:30 +0200

    Shape documentation

--- include/phys2d/colliders/Shape.h
+++ include/phys2d/colliders/Shape.h
@@ -10,11 +10,10 @@ namespace phys2d{
             CIRCLE=0, POLY=1
         };
 
-
         const Type type;
-        float maxExtent;
+        float maxExtent; // The maximum distance between the COM and the edge of the objedt
 
-        virtual Shape* clone()=0;
+        virtual Shape* clone()=0; // Returns a pointer to a copy of the object
 
         protected:
         Shape(Type type, float maxExtent);
@@ -22,21 +21,22 @@ namespace phys2d{
 
     struct ShapePoly : Shape {
         ShapePoly(std::initializer_list<Vec2> points);
+
         ShapePoly(float halfext=1.f); // Square
         ShapePoly(float halfextX, float halfextY); // Rect
 
-        const std::vector<Vec2> points; // OPTIMIZATION: data structure
-        const std::vector<Vec2> normals;
+        const std::vector<Vec2> points; 
+        const std::vector<Vec2> normals; // The face normals for the face composed of the points i - i + 1
 
-        Vec2 getMaxPoint(Vec2 dir) const;
+        Vec2 getMaxPoint(Vec2 dir) const; // Returns the point thats the furthest out in the direction dir
 
         Shape* clone() override;
 
         private:
-        std::vector<Vec2> getPoints(std::initializer_list<Vec2> points_);
+        std::vector<Vec2> getPoints(std::initializer_list<Vec2> points_); // Used by the constructor to get the points for the body
         std::vector<Vec2> getPoints(float halfextX, float halfextY);
 
-        std::vector<Vec2> calculateNormals();
+        std::vector<Vec2> calculateNormals(); // Used by the constructor to get normals for the body
     };
 
     struct ShapeCircle : Shape {
